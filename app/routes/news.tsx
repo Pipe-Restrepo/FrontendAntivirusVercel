97d148b58d63757431bb7ecd87822e3b9a73de27
Nataliaquinpo <nataliaquinpo@gmail.com>
2025-03-24 15:16:52 -0500

    feat(Novedades): Se separa el filtro a un componente

--- app/routes/news.tsx
+++ app/routes/news.tsx
@@ -3,13 +3,12 @@ import { useLoaderData } from "@remix-run/react";
 import { useState } from "react";
 import Carousel from "~/components/CarouselNews";
 import OpportunityCard from "~/components/OpportunityCard";
+import Filters from "~/components/Filters";
 import { api } from "~/service/api";
-import { HiMagnifyingGlass } from "react-icons/hi2";
 
 // Loader para obtener las oportunidades desde el backend
 export const loader: LoaderFunction = async ({ request }) => {
   try {
-    // Obtener las oportunidades de la API
     const opportunities = await api("/Opportunities", "GET", undefined, {}, request);
 
     if (!opportunities || !Array.isArray(opportunities)) {
@@ -17,15 +16,14 @@ export const loader: LoaderFunction = async ({ request }) => {
       return json([]);
     }
 
-    // Obtener los datos de cada institución en paralelo
     const updatedOpportunities = await Promise.all(
       opportunities.map(async (opportunity) => {
         try {
           const institution = await api(`/Institutions/${opportunity.institutionId}`, "GET", undefined, {}, request);
-          return { ...opportunity, institution }; // Agrega la institución a la oportunidad
+          return { ...opportunity, institution };
         } catch (error) {
           console.error(`Error obteniendo la institución ${opportunity.institutionId}:`, error);
-          return { ...opportunity, institution: null }; // En caso de error, asigna null
+          return { ...opportunity, institution: null };
         }
       })
     );
@@ -40,20 +38,23 @@ export const loader: LoaderFunction = async ({ request }) => {
 // Componente principal
 export default function News() {
   const opportunities = useLoaderData();
-  
-  // Estados para filtros
-  const [searchName, setSearchName] = useState("");
-  const [searchUbication, setSearchUbication] = useState("");
-  const [searchDate, setSearchDate] = useState("");
-  const [searchType, setSearchType] = useState("");
+
+  // Estado para filtros
+  const [filters, setFilters] = useState({
+    searchName: "",
+    searchUbication: "",
+    searchDate: "",
+    searchType: "",
+  });
 
   // Filtrar oportunidades
-  const filteredOpportunities = opportunities.filter(opportunity => {
+  const filteredOpportunities = opportunities.filter((opportunity) => {
     return (
-      (searchName === "" || opportunity.name.toLowerCase().includes(searchName.toLowerCase())) &&
-      (searchUbication === "" || (opportunity.institution?.ubication || "").toLowerCase().includes(searchUbication.toLowerCase())) &&
-      (searchDate === "" || (opportunity.adicionalDates || "").includes(searchDate)) &&
-      (searchType === "" || opportunity.type.toLowerCase().includes(searchType.toLowerCase()))
+      (filters.searchName === "" || opportunity.name.toLowerCase().includes(filters.searchName.toLowerCase())) &&
+      (filters.searchUbication === "" ||
+        (opportunity.institution?.ubication || "").toLowerCase().includes(filters.searchUbication.toLowerCase())) &&
+      (filters.searchDate === "" || (opportunity.adicionalDates || "").includes(filters.searchDate)) &&
+      (filters.searchType === "" || opportunity.type.toLowerCase().includes(filters.searchType.toLowerCase()))
     );
   });
 
@@ -63,82 +64,10 @@ export default function News() {
       <div className="relative flex justify-center w-3/4 h-60">
         <Carousel />
       </div>
-      <section className="space-y-3 shadow-lg border rounded-lg p-4 w-4/5">
-        {/* Campo de búsqueda por nombre */}
-        <div className="relative w-full">
-          <HiMagnifyingGlass className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-xl" />
-          <input
-            className="w-full h-10 pl-10 pr-3 border border-black rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
-            type="text"
-            placeholder="Busca tu próxima oportunidad"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-          />
-        </div>
-        {/* Filtros */}
-        <div className="w-full flex gap-4">
-          {/* Fecha */}
-          <div className="flex items-center border border-black rounded-md overflow-hidden w-1/3">
-            <span className="bg-gray-200 px-3 py-2 text-black font-semibold text-sm whitespace-nowrap">
-              ¡Filtra tu búsqueda!
-            </span>
-            <input
-              className="w-full h-10 px-3 border-l border-black focus:outline-none focus:ring-2 focus:ring-gray-400 text-gray-400"
-              type="date"
-              id="filtrar"
-              value={searchDate}
-              onChange={(e) => setSearchDate(e.target.value)}
-            />
-          </div>
 
-          {/* Ubicación */}
-          <div className="relative flex items-center border border-black rounded-md overflow-hidden w-1/3">
-            <span className="bg-gray-200 px-3 py-2 text-black font-semibold text-sm whitespace-nowrap">
-              Ubicación
-            </span>
-            <HiMagnifyingGlass className="absolute left-28 top-1/2 transform -translate-y-1/2 text-gray-400 text-xl" />
-            <input
-              className="w-full h-10 px-12 border-l border-black focus:outline-none focus:ring-2 focus:ring-gray-400"
-              type="text"
-              id="ubication"
-              placeholder="Busca tu ubicación"
-              value={searchUbication}
-              onChange={(e) => setSearchUbication(e.target.value)}
-            />
-          </div>
+      {/* Componente de filtros */}
+      <Filters onFilterChange={setFilters} />
 
-          {/* Tipo de oportunidad */}
-          <div className="relative flex items-center border border-black rounded-md overflow-hidden w-1/3">
-            <span className="bg-gray-200 px-3 py-2 text-black font-semibold text-sm whitespace-nowrap">
-              Tipo de oportunidad
-            </span>
-            <HiMagnifyingGlass className="absolute left-44 top-1/2 transform -translate-y-1/2 text-gray-400 text-xl" />
-            <input
-              className="w-full h-10 px-12 border-l border-black focus:outline-none focus:ring-2 focus:ring-gray-400"
-              type="text"
-              placeholder="Busca tu oportunidad"
-              id="typeOpportunity"
-              value={searchType}
-              onChange={(e) => setSearchType(e.target.value)}
-            />
-          </div>
-        </div>
-        {/* Botones */}
-        <div className="flex justify-end">
-          <button
-            className="w-1/6 bg-gray-400 font-black text-white p-2 rounded-lg font-bold hover:bg-yellow-600 mx-2"
-            type="button"
-            onClick={() => {
-              setSearchName("");
-              setSearchUbication("");
-              setSearchDate("");
-              setSearchType("");
-            }}
-          >
-            Limpiar
-          </button>
-        </div>
-      </section>
       {/* Oportunidades filtradas */}
       <section className="w-full md:w-11/12 min-h-[256px] flex-grow mb-44 px-4">
         <div className="grid grid-cols-4 gap-10 w-full">
