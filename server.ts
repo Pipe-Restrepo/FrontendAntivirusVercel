9ecdef065c4ad595623249de6c854745c0776110
Juan Felipe Restrepo <frestrepos.jfrs@gmail.com>
2025-07-07 12:34:30 -0500

    Fix routing configuration for Remix on Vercel

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { createRequestHandler } from "@remix-run/vercel";
 
-// Dinamically import the build to avoid TypeScript errors
-const build = await import("./build/server/index.js");
+// @ts-ignore
+import * as build from "./build/server/index.js";
 
 export default createRequestHandler({ build });
